faea60d7189d693030324627ff979084858de7a9
soham <sohambasak42@gmail.com>
2024-05-20 23:57:33 +0530

    feat(vote-service): auth middleware added in required routes

--- apps/backend/comment-service/src/routes/comment.route.ts
+++ apps/backend/comment-service/src/routes/comment.route.ts
@@ -6,13 +6,13 @@ import {
   updateCommentHandler,
 } from '../handler/comment.handler';
 import { createCommentValidator } from '../validations/comment.validation';
-import { Variables } from '@repo/auth-config';
+import auth, { Variables } from '@repo/auth-config';
 
 const router = new Hono<{ Variables: Variables }>();
 
-router.post('/create', createCommentValidator, createCommentHandler);
+router.post('/create', auth.withAuthMiddleware, createCommentValidator, createCommentHandler);
 router.get('/comments/:blogId', getAllCommentsHandler);
-router.post('/update', updateCommentHandler);
-router.delete('/delete', deleteCommentHandler);
+router.post('/update', auth.withAuthMiddleware, updateCommentHandler);
+router.delete('/delete', auth.withAuthMiddleware, deleteCommentHandler);
 
 export default router;
